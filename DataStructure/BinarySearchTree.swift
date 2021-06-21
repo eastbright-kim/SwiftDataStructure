7804ca99ace74c78a41eb0ecbbbfb2db9768a5f1
김동환 <1033513@naver.com>
2021-06-21 19:53:23 +0900

    BST naive insert func 추가

--- DataStructure/BinarySearchTree.swift
+++ DataStructure/BinarySearchTree.swift
@@ -7,9 +7,23 @@
 
 import Foundation
 
-enum BinaryTree<T> {
+enum BinaryTree<T: Comparable> {
     case empty
     
     //메모리에 값 타입 인스턴스 올릴 때, 정확히 얼마가 될 지 알아야하는데, 재귀가 생기면 크기가 얼마나 될 지 알 수 없어서 오류가 생김, indirect 써주면 reference가 올라가서 크기를 알 수 있어서 오류 해결
     indirect case node(BinaryTree, T, BinaryTree)
+    
+    mutating func naiveInsert(newValue: T){
+        guard case .node(var left, let value, var right) = self else {
+            self = .node(.empty, newValue, .empty)
+            return
+        }
+        
+        if newValue < value {
+            left.naiveInsert(newValue: newValue)
+        } else {
+            right.naiveInsert(newValue: newValue)
+        }
+        
+    }
 }
