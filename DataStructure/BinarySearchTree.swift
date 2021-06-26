6561716b45f293f4c55b933b4266113bf9cdeba2
김동환 <1033513@naver.com>
2021-06-26 20:23:23 +0900

    BinarySearchTree 구현

--- DataStructure/BinarySearchTree.swift
+++ DataStructure/BinarySearchTree.swift
@@ -27,3 +27,52 @@ enum BinaryTree<T: Comparable> {
         
     }
 }
+
+
+public class BinarySearchTree<T> {
+    
+    private(set) public var value: T
+    private(set) public var parent: BinarySearchTree?
+    private(set) public var left: BinarySearchTree?
+    private(set) public var right: BinarySearchTree?
+    
+    public init(value: T){
+        self.value = value
+    }
+    
+    public var isRoot: Bool {
+        return parent == nil
+    }
+    
+    public var isLeaf: Bool {
+        return left == nil && right == nil
+    }
+    
+    public var isLeftChild: Bool {
+        return parent?.left === self
+    }
+    
+    public var isRightChild: Bool {
+        return parent?.right === self
+    }
+    
+    public var hasLeftChild: Bool {
+        return left != nil
+    }
+    
+    public var hasRightChild: Bool {
+        return right != nil
+    }
+    
+    public var hasAnyChild: Bool {
+        return left != nil || right != nil
+    }
+    
+    public var hasBothChildren: Bool {
+        return left != nil && right != nil
+    }
+    
+    public var count: Int {
+        return (left?.count ?? 0) + 1 + (right?.count ?? 0)
+    }
+}
