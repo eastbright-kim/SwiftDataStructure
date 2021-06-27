1782f04cc67b448d97f3ca21b4cb0af6d2c724a4
김동환 <1033513@naver.com>
2021-06-27 15:42:41 +0900

    BST height, depth method 추가

--- DataStructure/BinarySearchTree.swift
+++ DataStructure/BinarySearchTree.swift
@@ -198,6 +198,26 @@ public class BinarySearchTree<T: Comparable> {
         return replacement
     }
     
+    public func height() -> Int {
+        
+        if isLeaf {
+            return 0
+        } else {
+            return 1 + max(left?.height() ?? 0, right?.height() ?? 0)
+        }
+    }
+    
+    public func depth() -> Int {
+        
+        var node = self
+        var edge = 0
+        
+        while let parent = node.parent {
+            node = parent
+            edge += 1
+        }
+        return edge
+    }
 }
 
 extension BinarySearchTree: CustomStringConvertible {
