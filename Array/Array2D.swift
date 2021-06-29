c069e37e8640b342ddba2572e470b2c82023504a
김동환 <1033513@naver.com>
2021-06-29 11:10:03 +0900

    2차원 배열 (repeating: , count: )

---
+++ Array/Array2D.swift
@@ -0,0 +1,12 @@
+//
+//  Array.swift
+//  DataStructure
+//
+//  Created by 김동환 on 2021/06/28.
+//
+
+import Foundation
+
+//2차원 배열 선언, 정의
+var cookies = [[Int]](repeating: [Int](repeating: 0, count: 7), count: 6)
+
